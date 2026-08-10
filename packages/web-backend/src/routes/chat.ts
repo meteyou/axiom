meteyou/openagent

feat(uploads): extract text content from PDF and text file uploads
- Add isExtractableType(), extractTextContent(), saveUploadWithExtraction()
  to packages/core/src/uploads.ts
- Add pdf-parse dependency for PDF text extraction (lazy-loaded)
- Export new functions from packages/core/src/index.ts
- Use saveUploadWithExtraction() in web chat upload route
- Inject extractedText into agent context for non-image uploads
- Add uploads-extraction.test.ts covering extraction logic

packages/web-backend/src/routes/chat.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import type { Database } from '@openagent/core'
-import { saveUpload, serializeUploadsMetadata } from '@openagent/core'
+import { saveUploadWithExtraction, serializeUploadsMetadata } from '@openagent/core'
 import { jwtMiddleware } from '../auth.js'
 import type { AuthenticatedRequest } from '../auth.js'
 import { uploadMiddleware } from '../uploads.js'
@@ -10,7 +10,7 @@ export function createChatRouter(db: Database): Router {
 
   router.use(jwtMiddleware)
 
-  router.post('/message', uploadMiddleware.array('files', 5), (req: AuthenticatedRequest, res) => {
+  router.post('/message', uploadMiddleware.array('files', 5), async (req: AuthenticatedRequest, res) => {
     const userId = req.user!.userId
     const text = typeof req.body?.content === 'string' ? req.body.content.trim() : ''
     const files = (req.files as Express.Multer.File[] | undefined) ?? []
@@ -21,14 +21,14 @@ export function createChatRouter(db: Database): Router {
     }
 
     const sessionId = `web-${userId}-${Date.now()}`
-    const uploads = files.map(file => saveUpload({
+    const uploads = await Promise.all(files.map(file => saveUploadWithExtraction({
       buffer: file.buffer,
       originalName: file.originalname,
       mimeType: file.mimetype,
       source: 'web',
       userId,
       sessionId,
-    }))
+    })))
 
     const metadata = uploads.length > 0 ? serializeUploadsMetadata(uploads) : null
     db.prepare(

meteyou/axiom

packages/web-backend/src/routes/chat.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import type { Database, AgentCore } from '@axiom/core'
-import { saveUpload, serializeUploadsMetadata } from '@axiom/core'
+import { saveUploadWithExtraction, serializeUploadsMetadata } from '@axiom/core'
 import { jwtMiddleware } from '../auth.js'
 import type { AuthenticatedRequest } from '../auth.js'
 import { uploadMiddleware } from '../uploads.js'
@@ -23,7 +23,7 @@ export function createChatRouter(options: ChatRouterOptions): Router {
 
   router.use(jwtMiddleware)
 
-  router.post('/message', uploadMiddleware.array('files', 5), (req: AuthenticatedRequest, res) => {
+  router.post('/message', uploadMiddleware.array('files', 5), async (req: AuthenticatedRequest, res) => {
     const userId = req.user!.userId
     const text = typeof req.body?.content === 'string' ? req.body.content.trim() : ''
     const files = (req.files as Express.Multer.File[] | undefined) ?? []
@@ -47,14 +47,14 @@ export function createChatRouter(options: ChatRouterOptions): Router {
     const session = agentCore.getSessionManager().getOrCreateSession(String(userId), 'web')
     const sessionId = session.id
 
-    const uploads = files.map(file => saveUpload({
+    const uploads = await Promise.all(files.map(file => saveUploadWithExtraction({
       buffer: file.buffer,
       originalName: file.originalname,
       mimeType: file.mimetype,
       source: 'web',
       userId,
       sessionId,
-    }))
+    })))
 
     const metadata = uploads.length > 0 ? serializeUploadsMetadata(uploads) : null
     db.prepare(